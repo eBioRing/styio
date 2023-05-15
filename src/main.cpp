8e94d596738cdf817e5f9fc9ff23874f50f122d7
Unka Y.Y <unka.malloc@outlook.com>
2023-05-15 22:19:43 +0100

    Tokenizer

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,14 @@
 #include <iostream>
 
 enum TokenType {
-  TOK_UNKOWN,
-  
-  TOK_EOF, // end-of-line
+  TOK_UNKNOWN,
 
-  TOK_SPACE, // space
+  TOK_SPACE,
+  
+  TOK_EOL, // \n
+  TOK_EOF, // EOF
 
   TOK_ID,
-
   TOK_INT,
   TOK_FLOAT,
 
@@ -22,18 +22,23 @@ enum TokenType {
   TOK_COLON, // :
   TOK_SEMICOLON, // ;
 
-  TOK_VARDEF, // @
+  TOK_TILDE, // ~
+  TOK_EXCLAM, // !
+  TOK_AT, // @
+  TOK_HASH, // #
+  TOK_DOLLAR, // $
+  TOK_PERCENT, // %
+  TOK_HAT, // ^
+  TOK_CHECK, // ?
+  TOK_SLASH, // /
+  TOK_BSLASH, // Back Slash
+  TOK_PIPE, // |
   
   TOK_ELLIPSIS, // ...
 
-  TOK_EXCLAM, // !
-
-  TOK_NEG, // -
-
-  TOK_ADD, // +
-  TOK_SUB, // -
-  TOK_MUL, // *
-  TOK_DIV, // /
+  TOK_SQUOTE, // '
+  TOK_DQUOTE, // "
+  TOK_BQUOTE, // `
 
   TOK_LPAREN, // (
   TOK_RPAREN, // )
@@ -46,11 +51,44 @@ enum TokenType {
 
   TOK_LANGBRAC, // <
   TOK_RANGBRAC, // >
+
+  TOK_AND, // &&
+  TOK_OR, // ||
+  TOK_XOR, // ^
+
+  TOK_BITAND, // &
+  TOK_BITOR, // |
+  TOK_BITXOR, // ^
+
+  TOK_LSHIFT, // <<
+  TOK_RSHIFT, // >>
+
+  TOK_NOT, // !
+
+  TOK_NEG, // -
+
+  TOK_ADD, // +
+  TOK_SUB, // -
+  TOK_MUL, // *
+  TOK_DIV, // /
+
+  TOK_GT, // >
+  TOK_GE, // >=
+  TOK_LT, // <
+  TOK_LE, // <=
+  TOK_EQ, // ==
+  TOK_NE, // !=
+
+  TOK_MATCH, // ?=
+
+  TOK_INFINITE_LIST, // [...]
 };
 
 // ?(a > 1) [true % false]
 
-static int matchToken() {
+static std::vector<int> Tokenize() {
+  std::vector<int> tokenBuffer;
+
   static int thisChar = ' ';
 
   thisChar = getchar();
@@ -61,43 +99,181 @@ static int matchToken() {
     thisChar = getchar();
   }
 
+  if (isalpha(thisChar) || thisChar == '_') {
+    std::string IdStr = "";
+    IdStr += thisChar;
+
+    // [a-zA-Z][a-zA-Z0-9_]*
+    while (isalnum((thisChar = getchar())) || thisChar == '_') 
+    {
+      IdStr += thisChar;
+    }
+
+    std::cout << "[INFO]: ID `" << IdStr << "`" << std::endl;
+
+    tokenBuffer.push_back(
+      TokenType::TOK_ID
+    );
+  }
+
+  if (isdigit(thisChar)) {
+    std::string numStr = "";
+    numStr += thisChar;
+    thisChar = getchar();
+
+    // [0-9]*
+      while (isdigit(thisChar))
+      {
+        numStr += thisChar;
+        thisChar = getchar();
+      };
+
+    if (thisChar == '.') {
+      numStr += thisChar;
+      thisChar = getchar();
+
+      while (isdigit(thisChar))
+      {
+        numStr += thisChar;
+        thisChar = getchar();
+      };
+
+      if (!isspace(thisChar)) {
+        std::cout << "[Error]: Float `" << numStr << "` ends with unexpected char `" << char(thisChar) << "`" << std::endl;
+      }
+
+      std::cout << "[INFO]: Float `" << numStr << "`" << std::endl;
+
+      tokenBuffer.push_back(
+        TokenType::TOK_FLOAT
+      );
+    } 
+    else 
+    {
+      if (!isspace(thisChar)) {
+        std::cout << "[Error]: Integer `" << numStr << "` ends with unexpected char `" << char(thisChar) << "`" << std::endl;
+      }
+
+      std::cout << "[INFO]: Integer `" << numStr << "`" << std::endl;
+
+      tokenBuffer.push_back(
+        TokenType::TOK_INT
+      );
+    }
+  }
+
   switch (thisChar)
   {
     case EOF:
-      return TokenType::TOK_EOF;
+      tokenBuffer.push_back(
+        TokenType::TOK_EOF
+      );
+      break;
+
+    case ',':
+      tokenBuffer.push_back(
+        TokenType::TOK_COMMA
+      );
+      break;
+      
+    case '.':
+      tokenBuffer.push_back(
+        TokenType::TOK_DOT
+      );
+      break;
+
+    case ';':
+      tokenBuffer.push_back(
+        TokenType::TOK_SEMICOLON
+      );
+      break;
+
+    case ':':
+      tokenBuffer.push_back(
+        TokenType::TOK_COLON
+      );
+      break;
+
+    case '!':
+      tokenBuffer.push_back(
+        TokenType::TOK_EXCLAM
+      );
+      break;
 
     case '(':
-      return TokenType::TOK_LPAREN;
+      tokenBuffer.push_back(
+        TokenType::TOK_LPAREN
+      );
+      break;
+
+    case ')':
+      tokenBuffer.push_back(
+        TokenType::TOK_RPAREN
+      );
+      break;
+
+    case '[':
+      tokenBuffer.push_back(
+        TokenType::TOK_LBOXBRAC
+      );
+      break;
+
+    case ']':
+      tokenBuffer.push_back(
+        TokenType::TOK_RBOXBRAC
+      );
+      break;
+
+    case '{':
+      tokenBuffer.push_back(
+        TokenType::TOK_LCURBRAC
+      );
+      break;
+
+    case '}':
+      tokenBuffer.push_back(
+        TokenType::TOK_RCURBRAC
+      );
+      break;
 
     case '@':
-      do
-      {
-        thisChar = getchar();
-      } while (isspace(thisChar));
+      thisChar = getchar();
+
+      if (thisChar != '\n' || thisChar != EOF) {
+        while (isspace(thisChar))
+        {
+          thisChar = getchar();
+        };
+      }
 
       // VAR_DEF := "@" "(" [<ID> ["," <ID>]*]? ")"
       if (thisChar == '(') {
-        return TokenType::TOK_VARDEF;
-      } else {
-        return TokenType::TOK_UNKOWN;
+        tokenBuffer.push_back(
+          TokenType::TOK_AT
+        );
+        break;
       }
 
     default:
-      std::cout << "[INFO]: unexpected char " << char(thisChar) << std::endl;
       break;
   }
 
-  return TokenType::TOK_UNKOWN;
+  return tokenBuffer;
 }
 
 int main() {
+
   while (1) 
   {
     fprintf(stderr, "</styio/> ");
 
-    int thisToken = matchToken();
+    for (int tok: Tokenize()) {
+      std::cout << tok << ' ';
+    };
 
-    std::cout << "[INFO]: Found " << thisToken << std::endl;
+    std::cout << std::endl;
+
+    // std::cout << "[INFO]: Token <" << thisToken << ">" << std::endl;
   };
 
   return 0;
